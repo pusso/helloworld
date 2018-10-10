de931c5fe0dc64e0a6e6a8e8190612911376d7ee
a.puskin <andreipuskin@gmail.com>
2018-10-10 23:36:50 +0300

    questions to curl

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@ using namespace std;
 
 int main(int argc, char const *argv[]) {
 	cout << "build " << version() << endl;
-	cout << "Hello, World!" << version() << endl;
+	cout << "Hello, World!" << endl;
 	return 0;
 }
