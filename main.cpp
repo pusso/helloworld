efdf27030a1e99a69d4757731b4c8a792a6262dc
a.puskin <andreipuskin@gmail.com>
2018-10-10 22:07:02 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "lib.h"
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+	std::cout << "Hello, World!" << std::endl;
+	std::cout << "Version " << version() << std::endl;
+	return 0;
+}
