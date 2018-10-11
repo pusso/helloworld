c5c215456acc89c7f6303ca495002a3017303ef4
a.puskin <andreipuskin@gmail.com>
2018-10-11 17:36:03 +0300

    main without args & binary file according to the technical task

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "lib.h"
 using namespace std;
 
-int main(int argc, char const *argv[]) {
+int main() {
 	cout << "build " << version() << endl;
 	cout << "Hello, World!" << endl;
 	return 0;
